15764ad76dd0e231c2c182d534c5a29c0a0e6a0c
Elmaaaaaaa <105375038+Elmaaaaaaa@users.noreply.github.com>
2022-05-12 18:06:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 num = input("Enter a word or a number to test for palindrome or 'exit' ")
-#if num == "exit":
-    #break
-
-#elif num.lowercase == num[::-1].lowercase:
-    #print("This is a palindrome!")
-#else:
-    #print("This is not a palindrome")
 
 for i in num:
     if num == "exit":
